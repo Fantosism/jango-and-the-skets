94528e6d1a9ce7f2d84cf7838fa510a8d53cf844
Bryan Johnson <itsme@bryanmakes.software>
2019-03-29 22:05:03 -0700

    combat integrated, hp counter

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -2,8 +2,12 @@ import libtcodpy as libtcod
 from random import randint
 
 from entity import Entity
+
+from components.ai import BasicMonster
+from components.fighter import Fighter
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from render_functions import RenderOrder
 
 
 class GameMap:
@@ -119,12 +123,34 @@ class GameMap:
                 [entity for entity in entities if entity.x == x and entity.y == y]
             ):
                 if randint(0, 100) < 80:
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
+
                     monster = Entity(
-                        x, y, "o", libtcod.desaturated_green, "Orcs", blocks=True
+                        x,
+                        y,
+                        "o",
+                        libtcod.desaturated_green,
+                        "Orcs",
+                        blocks=True,
+                        render_order=RenderOrder.ACTOR,
+                        fighter=fighter_component,
+                        ai=ai_component,
                     )
                 else:
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+
                     monster = Entity(
-                        x, y, "T", libtcod.darker_green, "Troll", blocks=True
+                        x,
+                        y,
+                        "T",
+                        libtcod.darker_green,
+                        "Troll",
+                        blocks=True,
+                        render_order=RenderOrder.ACTOR,
+                        fighter=fighter_component,
+                        ai=ai_component,
                     )
 
                 entities.append(monster)
