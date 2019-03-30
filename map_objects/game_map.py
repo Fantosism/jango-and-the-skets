cf3b3dacfa1e941d3defa17f131787d7db68de17
Bryan Johnson <itsme@bryanmakes.software>
2019-03-29 20:10:52 -0700

    placing enemies and kicking them

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,5 +1,7 @@
+import libtcodpy as libtcod
 from random import randint
 
+from entity import Entity
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 
@@ -17,12 +19,21 @@ class GameMap:
         return False
 
     def initialize_tiles(self):
-        tiles = [[Tile(True) for y in range(self.height)]
-                 for x in range(self.width)]
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    def make_map(
+        self,
+        max_rooms,
+        room_min_size,
+        room_max_size,
+        map_width,
+        map_height,
+        player,
+        entities,
+        max_monsters_per_room,
+    ):
         rooms = []
         num_rooms = 0
 
@@ -60,7 +71,7 @@ class GameMap:
 
                     # center coordinates of previous room
                     (prev_x, prev_y) = rooms[num_rooms - 1].center()
- 
+
                     # Coin flip
                     if randint(0, 1) == 1:
                         # first move horizontally than vertically
@@ -70,6 +81,9 @@ class GameMap:
                         # first move vertically, than horizontally
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
+
+                # Place monsters in room
+                self.place_entities(new_room, entities, max_monsters_per_room)
 
                 # finally, append the new room to the list
                 rooms.append(new_room)
@@ -91,3 +105,26 @@ class GameMap:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get a random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any(
+                [entity for entity in entities if entity.x == x and entity.y == y]
+            ):
+                if randint(0, 100) < 80:
+                    monster = Entity(
+                        x, y, "o", libtcod.desaturated_green, "Orcs", blocks=True
+                    )
+                else:
+                    monster = Entity(
+                        x, y, "T", libtcod.darker_green, "Troll", blocks=True
+                    )
+
+                entities.append(monster)
